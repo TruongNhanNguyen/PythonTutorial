8366622f1bbc0ad996a8abf2fbffee560e5ba23b
Truong Nhan <nhantruongnguyen123@gmail.com>
2021-12-05 21:36:23 +0700

    refactor(graph): minimum spanning tree (Prim-Jarnik Algorithm).

--- DataStructures/Graph/Graph.py
+++ DataStructures/Graph/Graph.py
@@ -276,3 +276,39 @@ def shorted_path_lengths(g, src):
                     pq.update(pqlocator[v], d[v], v)    # update the pq entry
 
     return cloud
+
+
+def MST_PrimJarnik(g):
+    """ Compute a minimum spanning tree of weighted graph g.
+    Return a list of edges that comprise the MST (in arbitrary order). """
+
+    d = {}                                  # d[v] is bound on distance to tree
+    tree = []                               # list of edges in spanning tree
+    pq = AdaptableHeap()                    # d[v] maps to value (v, e=(u, v))
+    pqlocator = {}                          # map from vertex to its pq locator
+
+    # for each vertex v of the graph, add an entry to the priority queue, with
+    # the source having distance 0 and all others having infinite distance
+    for v in g.vertices():
+        if len(d) == 0:                     # this is the first node
+            d[v] = 0                        # make it the root
+        else:
+            d[v] = float('inf')
+        pqlocator[v] = pq.add(d[v], (v, None))
+
+    while not pq.is_empty():
+        key, value = pq.remove_min()
+        u, edge = value                     # unpack tuple from pq
+        del pqlocator[u]                    # u is no longer in pq
+        if edge is not None:
+            tree.append(edge)               # add edge to tree
+        for link in g.incident_edges(u):
+            v = link.opposite(u)
+            if v in pqlocator:              # thus v not yet in tree
+                # see if edge (u, v) better connects v to the growing tree
+                wgt = link.element()
+                if wgt < d[v]:              # better edge to v
+                    d[v] = wgt              # update the distance
+                    pq.update(pqlocator[v], d[v], (v, link))    # update the pq entry
+
+    return tree
